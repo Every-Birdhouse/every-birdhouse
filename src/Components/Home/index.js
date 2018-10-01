be40f815cb9326cca37fddd7d16f6a4bda2b062e
Andrew Vye <a.vye26@gmail.com>
2018-10-01 12:21:19 -0700

    Update font and positioning tweaks to content

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -13,40 +13,45 @@ let logo = logo_yellow; // default it to logo_yellow
 
 export default function Home() {
   return (
-    <div className='home'>
+    <div>
       <Default>
-        <div className='videoContainer'>
-          <div className='video'>
-            <iframe
-              src='https://player.vimeo.com/video/267392220?title=0&byline=0&portrait=0&badge=0'
-              frameborder='0'
-              webkitallowfullscreen
-              mozallowfullscreen
-              allowfullscreen
-            />
-          </div>
-          <div className='videoTextContainer'>
-            <h4>Recent work: A Memory Drowned</h4>
-            <i class="fab fa-instagram" size="3x"></i>
-          </div>
+        <div className='leftContainer'>
+          <iframe
+            src='https://player.vimeo.com/video/289155044'
+            title='A Memory Drowned Trailer'
+            frameBorder='0'
+            height='750'
+            width='750'
+            webkitAllowFullScreen
+            mozAllowFullScreen
+            allowFullScreen
+          />
         </div>
-        <div className='linksContainer'>
-          <img src={logo} alt="Logo" onMouseOver={() => logo = logo_black} onMouseOut={() => logo = logo_yellow}/>
-          <h1 className="building">
-            <Link to='/film'>
-              FILM
-            </Link>
-          </h1>
-          <h1 className="building">
-            <Link to='/about'>
-              ABOUT
-            </Link>
-          </h1>
-          <h1 className="building">
-            <Link to='/contact'>
-              CONTACT
-            </Link>
-          </h1>
+        <div className='rightContainer'>
+          <img
+            src={logo}
+            alt='Every Birdhouse Media Logo'
+            className='logoImg'
+          />
+          <div className='aboutContainer'>
+            <p>
+              Ever curious filmmaker.<br/>
+              Director, Producer, Writer, Editor, and Colorist.<br/>
+              Currently based in Portland, OR.
+            </p>
+          </div>
+          <div className='socialMediaContainer'>
+            <p>say hello</p>
+            <a href='https://www.instagram.com/hello.arthurj/'>
+              <i className='fab fa-instagram socialMediaIcon'></i>
+            </a>
+            <a href='https://www.facebook.com/arthur.j.carueso'>
+              <i className='fab fa-facebook-square socialMediaIcon'></i>
+            </a>
+            <a href='https://vimeo.com/everybirdhousemedia'>
+              <i className='fab fa-vimeo-v socialMediaIcon'></i>
+            </a>
+          </div>
         </div>
       </Default>
       <Mobile>
